1fa465846d5fb4d8ddd3697428bed34dc6386673
Purnendu Kar <90676196+purnendu-pdgt@users.noreply.github.com>
2025-09-14 02:34:31 +0530

    feat: project initialisation

---
+++ src/main/java/com/example/ridehailingsystem/RidehailingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ridehailingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidehailingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidehailingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ridehailingsystem/RidehailingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ridehailingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidehailingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
