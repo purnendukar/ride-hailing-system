47cf3160e57bf0d615f5ba7c95b9d98aaca3ce37
Purnendu Kar <90676196+purnendu-pdgt@users.noreply.github.com>
2025-09-15 19:18:22 +0530

    chore: register repository

--- src/main/java/com/example/ridehailingsystem/repositories/RideRepository.java
+++ src/main/java/com/example/ridehailingsystem/repositories/RideRepository.java
@@ -2,6 +2,8 @@ package com.example.ridehailingsystem.repositories;
 
 import com.example.ridehailingsystem.models.Ride;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RideRepository extends JpaRepository<Ride, Long> {
 }
--- src/main/java/com/example/ridehailingsystem/repositories/VehicleRepository.java
+++ src/main/java/com/example/ridehailingsystem/repositories/VehicleRepository.java
@@ -2,6 +2,8 @@ package com.example.ridehailingsystem.repositories;
 
 import com.example.ridehailingsystem.models.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 }
